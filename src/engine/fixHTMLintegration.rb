c30fd71096c3d41272573865d3c3e8fa03545c0f
setscript <ponomarv-d@internet.ru>
2024-10-06 19:23:58 +0300

    add automod. add images

--- src/engine/fixHTMLintegration.rb
+++ src/engine/fixHTMLintegration.rb
@@ -9,10 +9,10 @@ end
 def convert_links_to_html(text)
   url_regex = /(https?:\/\/[^\s]+)/i
   hashtag_regex = /#(\w+)/ # Регулярное выражение для поиска хештегов
+  image_regex = /(https?:\/\/[^\s]+\.(?:jpg|jpeg|png|gif))/i # Регулярное выражение для изображений
   
   # Определяем общий цвет для ссылок и хештегов
   link_color = '#00bfff' # Ярко-голубой цвет
-
 
   # Заменяем код на специальный маркер
   text.gsub!(/<code>(.*?)<\/code>/, '[[CODE:\1]]')
@@ -21,7 +21,7 @@ def convert_links_to_html(text)
   if !text.include?('&lt;') && !text.include?('&gt;') && !text.include?('&amp;')
     # Экранируем текст от HTML/JS/CSS вставок
     text = escape_html(text)
-    
+
     # Экранируем символы < и > после обработки тегов
     text.gsub!('<', '&lt;')
     text.gsub!('>', '&gt;')
@@ -30,9 +30,13 @@ def convert_links_to_html(text)
   # Восстанавливаем код после экранирования
   text.gsub!(/\[\[CODE:(.*?)\]\]/, '<code>\1</code>')
 
-  # Заменяем ссылки на HTML
+  # Заменяем ссылки на HTML и обрабатываем изображения
   text = text.gsub(url_regex) do |url|
-    "<a href='#{url}' style='color: lightskyblue;'>#{url}</a>"
+    if url.match?(image_regex)
+      "<img src='#{url}' alt='Image' style='max-width:40%; height:auto;' />"
+    else
+      "<a href='#{url}' style='color: lightskyblue;'>#{url}</a>"
+    end
   end
 
   # Заменяем хештеги на HTML (без знака #)
@@ -40,10 +44,9 @@ def convert_links_to_html(text)
     "<span style='color: #{link_color}; font-weight: bold;'>#{hashtag}</span>"
   end
 
-
   text.gsub!(/\*\*(.+?)\*\*/, '<strong>\1</strong>')
   text.gsub!(/\`\`(.+?)\`\`/, '<code>\1</code>')
-
+  text.gsub!(/\_\_(.+?)\_\_/, '<i>\1</i>')
   if text.length > 147
     text = text.scan(/.{1,147}/).join("<br/>")
   end
--- src/forum.rb
+++ src/forum.rb
@@ -3,83 +3,139 @@ class Forum
 
   SECTIONS = %w[/b/ /m/ /tech/ /art/]
 
+  COMMON_RUSSIAN_WORDS = %w[и в не на с что я он она это как да ты был бы все]
+  
+  BLACKLISTED_WORDS = %w[группа реклама акция выигрыш подарок] # Пример черного списка
+
   def initialize
     @threads = []
     load_threads
   end
 
-
   def add_thread(title, section)
-    id = @threads.length # Use array length as unique identifier
+    id = @threads.length
     @threads << { id: id, title: title, section: section, comments: [], last_active: Time.now.iso8601 }
-    message = "🟢 *Yoda Alert!*\n\nБыл создан новый тред - #{title} (#{section})\n\nhttps://dassie-moral-gator.ngrok-free.app#{section}thread/#{id}   \n\n@Y0D9ch"
+    message = "🟢 *Yoda Alert!*\n\nБыл создан новый тред - #{title} (#{section})\n\nhttps://dassie-moral-gator.ngrok-free.app#{section}thread/#{id}\n\n@Y0D9ch"
+
+    send_telegram_message(message, false) # Отправляем в обычный чат
     
-    Telegram::Bot::Client.run(File.read("bot.token")) do |bot|
-      bot.api.send_message(chat_id: File.read("bot.chatid"), text: message, parse_mode: 'Markdown')
-    end
-    
-    puts "Adding thread: #{title} in section: #{section}" # Debugging output
+    puts "Добавляем тред: #{title} в раздел: #{section}" # Отладочный вывод
     save_threads
-    id # Return the new thread's identifie
+    id
   end
   
   def add_comment(thread_id, comment)
     thread_index = @threads.find_index { |thread| thread[:id] == thread_id }
-  
+
     if thread_index
-      # Получаем последние два комментария
-      recent_comments = @threads[thread_index][:comments].last(2)
-  
-      # Проверяем, если есть хотя бы два комментария и они одинаковые
-      if recent_comments.size == 2 && recent_comments.all? { |c| c == comment }
-        # Проверяем, был ли уже отправлен сигнал о спаме
-        if @threads[thread_index][:spam_alert_sent].nil? || !@threads[thread_index][:spam_alert_sent]
-          # Отправляем уведомление о спаме
-          message = "🚫 *Yoda Alert!*\n\nАвтомод среагировал на рейд. Рейд был отбит.\n\n@Y0D9ch"
-          Telegram::Bot::Client.run(File.read("bot.token")) do |bot|
-            bot.api.send_message(chat_id: File.read("bot.chatid"), text: message, parse_mode: 'Markdown')
-          end
-          
-          # Устанавливаем флаг, что уведомление о спаме было отправлено
-          @threads[thread_index][:spam_alert_sent] = true
-        end
-  
-        # Игнорируем комментарий, если он повторяется более двух раз подряд
+      if spam_detected?(thread_index, comment)
         return
-      else
-        # Сбрасываем флаг, если комментарий не является спамом
-        @threads[thread_index][:spam_alert_sent] = false
       end
-  
-      # Добавляем комментарий и обновляем время последней активности
+
       @threads[thread_index][:comments] << comment
       @threads[thread_index][:last_active] = Time.now.iso8601 
       save_threads
-  
-      # Подготавливаем сообщение
-      file = File.read('threads.json')
-      data = JSON.parse(file)
-  
-      # Указываем ID для поиска
-      id_to_find = 12
-  
-      # Находим раздел для указанного ID
-      entry = data.find { |item| item["id"] == id_to_find }
-  
-      message = "🟢 *Yoda Alert!*\n\nБыл создан новый комментарий: ```\n#{comment}\n```\n\n[>>> Перейти к треду](https://dassie-moral-gator.ngrok-free.app#{entry["section"]}thread/#{thread_id})\n\n@Y0D9ch"
-  
-      Telegram::Bot::Client.run(File.read("bot.token")) do |bot|
-        bot.api.send_message(chat_id: File.read("bot.chatid"), text: message, parse_mode: 'Markdown')
-      end
-  
+      
+      message = "🟢 *Yoda Alert!*\n\nБыл создан новый комментарий: ```\n#{comment}\n```\n\n[>>> Перейти к треду](https://dassie-moral-gator.ngrok-free.app#{@threads[thread_index][:section]}thread/#{thread_id})\n\n@Y0D9ch"
+      send_telegram_message(message, false) # Отправляем в обычный чат
+      
     else
-      puts "Invalid thread id: #{thread_id}" # Отладочный вывод
+      puts "Неверный идентификатор треда: #{thread_id}" # Отладочный вывод
     end
   end
-  
-  
-  
+
   private
+
+  def send_telegram_message(message, raid_alert = false)
+    chat_id = raid_alert ? File.read("bot.chid") : File.read("bot.chatid") # Замените на ID другого чата
+
+    Telegram::Bot::Client.run(File.read("bot.token")) do |bot|
+      bot.api.send_message(chat_id: chat_id, text: message, parse_mode: 'Markdown')
+    end
+  end
+
+  def spam_detected?(thread_index, comment)
+    normalized_comment = comment.downcase # Приводим комментарий к нижнему регистру
+
+    return false if contains_url?(normalized_comment) # Игнорируем комментарии с URL
+
+    recent_comments = @threads[thread_index][:comments].last(2)
+
+    # Проверка на повторяющиеся комментарии
+    if recent_comments.size == 2 && recent_comments.all? { |c| c.downcase == normalized_comment }
+      handle_spam_alert(thread_index)
+      return true
+    end
+
+    # Проверка на наличие черных слов и паттернов спама
+    if contains_blacklisted_words?(normalized_comment) || floating_point_spam?(normalized_comment) || repeated_character?(normalized_comment) || contains_repeated_patterns?(normalized_comment) || floating_point_with_text?(normalized_comment)
+      handle_spam_alert(thread_index)
+      return true
+    end
+    
+    false 
+  end
+
+  def handle_spam_alert(thread_index)
+    current_time = Time.now
+
+    last_spam_alert_time = @threads[thread_index][:last_spam_alert_sent]
+    
+    # Приводим last_spam_alert_time к объекту Time, если это строка
+    last_spam_alert_time = Time.parse(last_spam_alert_time) if last_spam_alert_time.is_a?(String)
+
+    if last_spam_alert_time.nil? || (current_time - last_spam_alert_time > 300)
+      message = "🚫 *Yoda Alert!*\n\nАвтомод среагировал на рейд. Рейд был отбит.\n\n@Y0D9ch | @Y0D9alerts"
+      send_telegram_message(message, true) # Отправляем только рейд-алерт в канал
+
+      @threads[thread_index][:last_spam_alert_sent] = current_time.iso8601 # Сохраняем как строку в ISO формате
+    end
+  end
+
+  def contains_blacklisted_words?(comment)
+    BLACKLISTED_WORDS.any? { |word| comment.include?(word) }
+  end
+
+  def contains_url?(comment)
+    # Проверяем наличие URL в комментарии (нечувствительно к регистру)
+    comment.match?(/https?:\/\/[\S]+/) || comment.match?(/www\.[\S]+/)
+  end
+
+  def random_number?(comment)
+    comment.match?(/^\d+$/) || comment.match?(/^(?:\d+\s*){3,}$/) 
+  end
+
+  def nonsensical_message?(comment)
+    words = comment.split(/\s+/).map(&:downcase)
+    
+    meaningful_words_count = words.count { |word| !COMMON_RUSSIAN_WORDS.include?(word) }
+    
+    meaningful_words_count < 2 # Считаем бессмысленным, если найдено менее двух значимых слов
+  end
+
+  def repeated_character?(comment)
+    return false if comment.empty?
+    
+    comment.chars.uniq.length == 1 # Если все символы одинаковые, длина уникальных символов будет равна 1.
+  end
+
+  def floating_point_spam?(comment)
+    has_floating_point_numbers = comment.scan(/\d+\.\d+/).any?
+    
+    has_repeated_sequences = comment.match?(/([A-Za-z])\1{2,}/)
+
+    has_floating_point_numbers && has_repeated_sequences
+  end
+  
+  def contains_repeated_patterns?(comment)
+    # Проверка на наличие повторяющихся паттернов (например, "abcabc" или "123123")
+    comment.match?(/(.+)\1/)
+  end
+  
+  def floating_point_with_text?(comment)
+    # Проверяем наличие чисел с плавающей запятой рядом с текстом (например, "0.631464 РЕДВИЖН")
+    comment.match?(/\d+\.\d+\s+[A-Za-zА-Яа-я]+/)
+  end
 
   def save_threads
     File.open('threads.json', 'w') do |file|
@@ -90,10 +146,11 @@ class Forum
   def load_threads
     if File.exist?('threads.json')
       @threads = JSON.parse(File.read('threads.json'), symbolize_names: true)
-      # Ensure all threads have a last_active value
       @threads.each do |thread|
-        thread[:last_active] ||= Time.now.iso8601 # Set to current time if nil
+        thread[:last_active] ||= Time.now.iso8601 
+        thread[:spam_alert_sent] ||= false 
+        thread[:last_spam_alert_sent] ||= nil # Инициализируем переменную для времени последнего сообщения о спаме
       end
     end
-  end
+end
 end
